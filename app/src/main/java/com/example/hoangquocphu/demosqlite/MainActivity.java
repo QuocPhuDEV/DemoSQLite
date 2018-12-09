f8d8dde6a86467d9793e531561eab8e5b71b2105
QuocPhuDEV <phuhq.it@gmail.com>
2018-12-09 21:32:20 +0700

    next

--- app/src/main/java/com/example/hoangquocphu/demosqlite/MainActivity.java
+++ app/src/main/java/com/example/hoangquocphu/demosqlite/MainActivity.java
@@ -8,7 +8,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.hoangquocphu.demosqlite.Customer.Cus_Activity;
+import com.example.hoangquocphu.demosqlite.Customer.Cus_List_Activity;
+import com.example.hoangquocphu.demosqlite.QRCode.Scan_Activity;
+import com.example.hoangquocphu.demosqlite.Question.AddQuestion;
 import com.example.hoangquocphu.demosqlite.Question.Question_List_Activity;
+import com.example.hoangquocphu.demosqlite.Questions.Ques_AddNew_Activity;
+import com.example.hoangquocphu.demosqlite.Questions.Ques_List_Activity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,8 +37,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.itemNewServey){
+        if (id == R.id.itemNewServey) {
             Intent intent = new Intent(MainActivity.this, Cus_Activity.class);
+            startActivity(intent);
+            return false;
+        } else if (id == R.id.itemCustomerList) {
+            Intent intent = new Intent(MainActivity.this, Cus_List_Activity.class);
+            startActivity(intent);
+            return false;
+        } else if (id == R.id.itemQuestionList){
+            Intent intent = new Intent(MainActivity.this, Ques_List_Activity.class);
+            startActivity(intent);
+            return false;
+        }else if (id == R.id.itemNewQuestion){
+            Intent intent = new Intent(MainActivity.this, Ques_AddNew_Activity.class);
+            startActivity(intent);
+            return false;
+        }else if (id == R.id.itemScanCustomer){
+            Intent intent = new Intent(MainActivity.this, Scan_Activity.class);
             startActivity(intent);
             return false;
         }
