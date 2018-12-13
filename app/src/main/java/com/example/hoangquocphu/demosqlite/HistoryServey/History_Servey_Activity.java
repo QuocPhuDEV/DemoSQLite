ad190158822de9978f9b0746aac8e4b4415d419c
QuocPhuDEV <phuhq.it@gmail.com>
2018-12-13 11:33:20 +0700

    thursday_13122018

---
+++ app/src/main/java/com/example/hoangquocphu/demosqlite/HistoryServey/History_Servey_Activity.java
@@ -0,0 +1,15 @@
+package com.example.hoangquocphu.demosqlite.HistoryServey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hoangquocphu.demosqlite.R;
+
+public class History_Servey_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history__servey_);
+    }
+}
